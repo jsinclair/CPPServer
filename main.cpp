ca8d4c977b9902f8683f405b5f90fbc586cb224a
James Sinclair <james@overtake.co.za>
2017-06-15 14:18:16 +0200

    Updated main.cpp to be inline with CGI stuff.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,28 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 #include "HelloClass.h"
 
 int main(int argc, char **argv)
 {
-	printf("hello world\n");
+	/*printf("hello world\n");
     
     HelloClass *testHello = new HelloClass(5);
     testHello->countToTarget();
     delete testHello;
     
     HelloClass dynam(10);
-    dynam.countToTarget();
+    dynam.countToTarget();*/
+    
+    cout << "Content-type:text/html\r\n\r\n";
+    cout << "<html>\n";
+    cout << "<head>\n";
+    cout << "<title>Hello World - First CGI Program</title>\n";
+    cout << "</head>\n";
+    cout << "<body>\n";
+    cout << "<h2>Hello World! This is my first CGI program</h2>\n";
+    cout << "</body>\n";
+    cout << "</html>\n";
     
 	return 0;
 }
