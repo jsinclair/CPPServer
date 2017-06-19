baa2f5f2fe58f9bed7ce3021432e0c66db9afd2a
James Sinclair <james@overtake.co.za>
2017-06-19 08:32:57 +0200

    Changed for ego.

--- main.cpp
+++ main.cpp
@@ -28,10 +28,10 @@ int main(int argc, char **argv)
     cout << "Content-type:text/html\r\n\r\n";
     cout << "<html>\n";
     cout << "<head>\n";
-    cout << "<title>Hello World - First CGI Program</title>\n";
+    cout << "<title>Hello CGI</title>\n";
     cout << "</head>\n";
     cout << "<body>\n";
-    cout << "<h2>Hello World! This is my first CGI program</h2>\n";
+    cout << "<h2>Hello World!</h2>\n";
     cout << "<p>";
     
     dynam.countToTarget();
