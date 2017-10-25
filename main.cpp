7dfd151bdd43dc59b091144ce193705eff65717a
James Sinclair <james@overtake.co.za>
2017-10-25 12:29:37 +0200

    Testing public folder

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@ using namespace jag;
 
 int main(int argc, char **argv)
 {
-    pdf::Document doc(pdf::create_file("hello.pdf"));
+    pdf::Document doc(pdf::create_file("./public/hello.pdf"));
     doc.page_start(597.6, 848.68);
     doc.page().canvas().text(50, 800, "Hello, world!");
     doc.page_end();
