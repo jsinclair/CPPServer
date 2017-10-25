e39c7c1ccb06f2a52149bb4c36a293064e5aae5e
James Sinclair <james@overtake.co.za>
2017-10-25 12:07:39 +0200

    Testing compiling.

--- main.cpp
+++ main.cpp
@@ -32,11 +32,11 @@ using namespace jag;
 
 int main(int argc, char **argv)
 {
-    pdf::Document doc(pdf::create_file("hello.pdf"));
+    /*pdf::Document doc(pdf::create_file("hello.pdf"));
     doc.page_start(597.6, 848.68);
     doc.page().canvas().text(50, 800, "Hello, world!");
     doc.page_end();
-    doc.finalize();
+    doc.finalize();*/
 //    return 0;
 
     
